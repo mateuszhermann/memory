e298a68a47f5a04ab73b30e5355c9d1b1ab55645
Krollka <kroll-marika@outlook.com>
2023-02-21 18:30:35 +0100

    Basic start screen

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1440,
+    height: 960,
+    resizable:false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
@@ -19,8 +20,6 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
