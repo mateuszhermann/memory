0c8ddf99ce1ad66c6c102129dacc600ce4601e67
mateuszhermann <majonez15066@wp.pl>
2023-02-24 21:39:51 +0100

    zmieniono:       mydb.sqlite 	nowy plik:       src/assets/arrowcircleleft2.png 	zmieniono:       src/gameScreen.css 	zmieniono:       src/gameScreen.html 	zmieniono:       src/gameScreenController.js

--- src/gameScreenController.js
+++ src/gameScreenController.js
@@ -6,22 +6,31 @@ let limiter=[];
 let active1=null;
 let active2=null;
 
-const marked=[];
-
+let marked=[];
+let limit;
 
 function clear(){
-    var cards=document.getElementsByTagName('img');
+    var cards=document.getElementsByClassName('card');
 
     Array.from(cards).forEach(card=>{
-        marked.forEach(marker=>{
-            if(card.alt!=marker){
-                console.log(marker)
-                card.src=bg;
-            }
-        })
-    })
+        if (!marked.includes(card.alt)) {
+            card.src=bg;
+        }
+    });
 }
-
+function endcheck(){
+    var cards=document.getElementsByClassName('card');
+    let counter=0;
+    Array.from(cards).forEach(card=>{
+        if(card.getAttribute('src')!=bg){
+            counter++;
+            console.log(`counter=${counter}`)
+        };
+        if(counter==limit){
+            window.location.replace('./gameOver.html')
+        }
+    });
+}
 function activate(e){
     var card=e.target;
 
@@ -41,7 +50,8 @@ function activate(e){
             console.log("marking");
             marked.push(card.alt);
         }
-        clear();
+        endcheck();
+        setTimeout(clear, 500);
         active1=null;
         active2=null;
     }
@@ -78,7 +88,7 @@ ipcRenderer.on('cards-data-response', (event, data) => {
         cards.push(card);
     })
     console.log(cards);
-    const limit=4;
+    limit=8;
     
     for(var i=0; i<limit;i++){
         limiter[i]=cards[i];
@@ -87,8 +97,26 @@ ipcRenderer.on('cards-data-response', (event, data) => {
     console.log(limiter);
 
     for(var i=0; i<limiter.length;i++){
-        playmat.innerHTML+=`<img src='${bg}' alt='${limiter[i].card_id}' id='${i}'>`;
+        playmat.innerHTML+=`<img src='${bg}' alt='${limiter[i].card_id}' class='card' id='${i}'>`;
         console.log('git');
+    };
+
+
+    if(limit<=4){
+        playmat.style.gridTemplateColumns = 'repeat(2, auto)';
+        const elements = document.getElementsByClassName('card');
+        playmat.style.width="420px"
+        for (let i = 0; i < elements.length; i++) {
+          elements[i].style.width = '200px';
+        }
+    };
+
+
+    if(limit>=16){
+        playmat.style.gridTemplateColumns = 'repeat(6, auto)';
+        playmat.style.width="915px"
+
+        
     };
     const images = document.getElementsByTagName('img');
     Array.from(images).forEach(img => {
